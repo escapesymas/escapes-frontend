f14bfb84aa640750cad9bf61b17fb43cbb36eac9
escapesymas <info@escapesymas.com>
2026-01-22 08:19:44 +0000

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -6,7 +6,7 @@
  * Set this in Vercel Dashboard: Settings > Environment Variables
  */
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
